373943705cab39ef6aaa294f143e45e4e8d365a4
Anton <oshurekdev@proton.me>
2024-10-19 13:32:48 +0200

    add some maps info

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,6 +19,12 @@ export default defineConfig({
             { label: "Деловой центр", slug: "maps/dc" },
             { label: "Мидтаун", slug: "maps/midtown" },
             { label: "Взрыв в городе", slug: "maps/wwg" },
+            { label: "Метро", slug: "maps/metro" },
+            { label: "Буря в пустыне", slug: "maps/sand-storm" },
+            { label: "Прованс", slug: "maps/provance" },
+            { label: "Убежище", slug: "maps/shelter" },
+            { label: "Наблюдательный пост", slug: "maps/observation-post" },
+            { label: "Пригород Багдада", slug: "maps/suburb-of-baghdad" },
           ],
         },
         {
