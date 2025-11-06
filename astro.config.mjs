b6385ad758a4c9e978af74bbc5a7cd8545649990
Anton Oshurek <oshurekdev@proton.me>
2025-11-06 23:44:23 +0100

    translate to russia and add basic template for clans

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,40 +3,48 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://pointblank-info.com",
-  integrations: [
-    starlight({
-      title: "PointBlank Info",
-      social: [
-      { icon: 'github', label: 'GitHub', href: 'https://github.com/AntonOshurek/pointblank-info.com' },
-      { icon: 'linkedin', label: 'Linkedin', href: 'https://www.linkedin.com/in/anton-oshurek/' },
-      ],
-      sidebar: [
-        {
-          label: "Карты",
-          items: [
-            // Each item here is one entry in the navigation menu.
-            { label: "Люксвиль", slug: "maps/luxvile" },
-            { label: "Деловой центр", slug: "maps/dc" },
-            { label: "Мидтаун", slug: "maps/midtown" },
-            { label: "Взрыв в городе", slug: "maps/wwg" },
-            { label: "Метро", slug: "maps/metro" },
-            { label: "Буря в пустыне", slug: "maps/sand-storm" },
-            { label: "Прованс", slug: "maps/provance" },
-            { label: "Убежище", slug: "maps/shelter" },
-            { label: "Наблюдательный пост", slug: "maps/observation-post" },
-            { label: "Пригород Багдада", slug: "maps/suburb-of-baghdad" },
-          ],
-        },
-        {
-          label: "Звания",
-          autogenerate: { directory: "ranks" },
-        },
-        {
-          label: "Скилы",
-          autogenerate: { directory: "skills" },
-        },
-      ],
-    }),
-  ],
+    site: "https://pointblank-info.com",
+    integrations: [
+        starlight({
+            title: "PointBlank Info",
+            social: [
+            { icon: 'github', label: 'GitHub', href: 'https://github.com/AntonOshurek/pointblank-info.com' },
+            { icon: 'linkedin', label: 'Linkedin', href: 'https://www.linkedin.com/in/anton-oshurek/' },
+            ],
+            locales: {
+                root: { label: 'Русский', lang: 'ru' },
+            },
+            defaultLocale: 'root',
+            sidebar: [
+                {
+                  label: "Карты",
+                  items: [
+                    // Each item here is one entry in the navigation menu.
+                    { label: "Люксвиль", link: "maps/luxvile" },
+                    { label: "Деловой центр", link: "maps/dc" },
+                    { label: "Мидтаун", link: "maps/midtown" },
+                    { label: "Взрыв в городе", link: "maps/wwg" },
+                    { label: "Метро", link: "maps/metro" },
+                    { label: "Буря в пустыне", link: "maps/sand-storm" },
+                    { label: "Прованс", link: "maps/provance" },
+                    { label: "Убежище", link: "maps/shelter" },
+                    { label: "Наблюдательный пост", link: "maps/observation-post" },
+                    { label: "Пригород Багдада", link: "maps/suburb-of-baghdad" },
+                  ],
+                },
+                {
+                  label: "Звания",
+                  autogenerate: { directory: "ranks" },
+                },
+                {
+                  label: "Скилы",
+                  autogenerate: { directory: "skills" },
+                },
+                {
+                  label: "Кланы",
+                  autogenerate: { directory: "clans" },
+                },
+            ],
+        }),
+    ],
 });
